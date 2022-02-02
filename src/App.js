f22897dd76da4dbdb6da12b2292331040bf3e16b
egsu <egsudotnet@gmail.com>
2022-02-02 16:11:16 +0700

    33 - Implementasi CRUD di React Native (Read - Method GET)

--- src/App.js
+++ src/App.js
@@ -74,3 +74,5 @@ export default app;
 //https://www.youtube.com/watch?v=tCD523C51_A&list=PLU4DS8KR-LJ3SP3PpRb870UoT_0_rjLpV&index=33 31 - Menyiapkan UI Component untuk Implementasi Proses CRUD
 
 //https://www.youtube.com/watch?v=aD6Jhs3CMuc&list=PLU4DS8KR-LJ3SP3PpRb870UoT_0_rjLpV&index=34 32 - Implementasi CRUD di React Native (Create - Method POST)
+
+// https://www.youtube.com/watch?v=LVNxKZxYOD0&list=PLU4DS8KR-LJ3SP3PpRb870UoT_0_rjLpV&index=35 33 - Implementasi CRUD di React Native (Read - Method GET)
